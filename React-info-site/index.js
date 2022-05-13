9ac457f65814428c0c92d6c98b92ca7bc2f95030
Abdelhamid Khamis <53490154+Abdelhamid-khamis@users.noreply.github.com>
2022-05-13 02:13:18 +0200

    building React info site

---
+++ React-info-site/index.js
@@ -0,0 +1,12 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+// ReactDOM - Library `numpy`
+// .render() - Method `.array()
+ReactDOM.render(
+    <ul>Wilkommen!<li>Guten Morgen</li><li>Gute Nacht</li></ul>, 
+    document.querySelector("#root")
+    )
